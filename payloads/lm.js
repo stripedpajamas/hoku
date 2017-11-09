61df096b682a3258ba23870054b1611828bf7000
Peter Squicciarini <stripedpajamas273@gmail.com>
2017-11-09 18:13:36 -0500

    Don't use localhost

--- payloads/lm.js
+++ payloads/lm.js
@@ -1,5 +1,5 @@
 module.exports = `
-#curl -sL hoku.me/assets/lawnmower > /tmp/x.png
-curl -sL localhost:3000/assets/lawnmower > /tmp/x.png
+curl -sL hoku.me/assets/lawnmower > /tmp/x.png
+#curl -sL localhost:3000/assets/lawnmower > /tmp/x.png
 osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/tmp/x.png"'
 `
